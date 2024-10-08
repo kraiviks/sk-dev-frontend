3f7e02d0a08aea435db91609fb97160f610d8b89
kraiviks <kraiviks@gmail.com>
2024-10-08 19:49:58 +0300

    chore: remove unused elements

--- src/components/sections/section-main/section-main.tsx
+++ src/components/sections/section-main/section-main.tsx
@@ -15,9 +15,7 @@ export const SectionMain = () => {
 		<section className="relative flex justify-center items-center h-screen pt-10 pb-10 overflow-hidden">
 			<Container className="flex flex-col w-full px-16">
 				<div className="flex flex-col sm:flex-row items-center sm:justify-between gap-10 sm:gap-20">
-					{/* Center */}
 					<div className="flex flex-col items-center w-full gap-7">
-						{/* <Title text="Developer" /> */}
 						<MainText
 							text={[
 								{
@@ -40,8 +38,6 @@ export const SectionMain = () => {
 						<MainParagraph />
 						<MainButton />
 					</div>
-					{/* Right */}
-					{/* <InfoList className="order-3 max-w-full" /> */}
 				</div>
 			</Container>
 			<Scroll className="absolute bottom-[5%] left-[50%] right-[50%] -translate-x[50%]" />
