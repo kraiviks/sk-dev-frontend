669fd536f222bee193e31e93111ff92bf7dfdb16
kraiviks <kraiviks@gmail.com>
2024-10-10 16:50:21 +0300

    style: added animation on h1 for articles page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,5 +1,6 @@
 import { Api } from '@/services/api/api-client';
 import { ArticleList, CreateArticleLink } from '@/components/shared';
+import * as motion from 'framer-motion/client';
 
 const ArticlesPage = async () => {
 	const { data } = await Api.getArticles();
@@ -7,7 +8,19 @@ const ArticlesPage = async () => {
 	return (
 		<div className="mt-16 container mx-auto py-12 flex flex-col gap-8">
 			<div className="flex justify-between items-center">
-				<h1 className="text-4xl font-bold">Articles</h1>
+				<motion.h1
+					className="text-4xl font-bold"
+					whileInView={{ opacity: [0, 1], scale: [0.5, 1] }}
+					viewport={{ once: true }}
+					transition={{
+						type: 'spring',
+						stiffness: 100,
+						damping: 10,
+						duration: 0.6,
+					}}
+				>
+					Articles
+				</motion.h1>
 				<CreateArticleLink />
 			</div>
 			<ArticleList initialData={data} />
